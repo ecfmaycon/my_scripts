03f4c21ff3ebffec53f7d74dea64000481249c0c
ecfmaycon <suporte.13@ecfconsultoria.com.br>
2026-04-17 14:41:53 -0300

    Update painel.user.js

--- painel.user.js
+++ painel.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Painel Prompts
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.5
 // @description  Painel multifuncional de prompts com visual escuro, gradiente, Auto-Enter, gerador SEO, interface móvel e imagens fixas em 1200x1200.
 // @author       maycon
 // @match        https://gemini.google.com/*
